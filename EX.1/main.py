8fcac926453048b3b2ec7d2f8e0094f00d004def
MALY <malyfertig@gmail.com>
2025-04-04 11:50:29 +0300

    fixed logic and add comment

--- EX.1/main.py
+++ EX.1/main.py
@@ -1,30 +1,53 @@
-import heapq
-from heapq import *
+from collections import Counter
+from heapq import heappush, heappop, heapify
 from itertools import islice
 
-error_counts = {}
+# Generator function that produces parts of the file
+def read_file_in_chunks(file_path, chunk_size):
+    with open(file_path, 'r') as file:
+        while True:
+            lines = [file.readline().strip() for _ in range(chunk_size)]
+            if all(line == '' for line in lines):
+                break
+            yield lines
 
-def find_n_common_errors(log_file, N):
-    cnt = 0
-    with open(log_file, 'r') as file:
-        line = file.readline()
-        while line and cnt < 20:
-            line = file.readline()
-            cnt += 1
-            underscore_index = line.rfind('_')
-            if underscore_index != -1 :
-                num_error = line[underscore_index + 1: -1]
+# Counts errors from blocks
+def count_errors_in_block(lines):
+    error_counter = Counter()
+    for line in lines:
+        if 'Error: ' in line:
+            error_code = line.split('Error: ')[1].strip()
+            error_counter[error_code] += 1
+    return error_counter
 
-                error_counts[num_error] = error_counts[num_error] + 1 if num_error in error_counts else 1
+# Main function to find the common errors n top
+def process_log_file(file_path, chunk_size, N):
+    error_count = Counter()
 
-    min_heap = [(value, key) for key, value in islice(error_counts.items(), N)]
+    # Divide into blocks
+    for chunk in read_file_in_chunks(file_path, chunk_size):
+        # Error count on each block
+        block_counter = count_errors_in_block(chunk)
 
-    for key, value in islice(error_counts.items(), N, None):
+        # Update the General Counts
+        error_count.update(block_counter)
+
+    # Find the most common errors
+    min_heap = [(value, key) for key, value in islice(error_count.items(), N)]
+    heapify(min_heap)
+
+    for key, value in islice(error_count.items(), N, None):
         if value > min_heap[0][0]:
             heappop(min_heap)
             heappush(min_heap, (value, key))
 
-
     return [error_number for value, error_number in min_heap]
 
-print(find_n_common_errors("C:\\Users\\f0527\\Downloads\\logs.txt",3))
+# Calling a function with the file, block size, and N most common errors
+file_path = 'C:/Users/f0527/Downloads/logs.txt'
+chunk_size = 1000 # Number of lines in the block
+top_n = 3 # The most common errors
+
+top_errors = process_log_file(file_path, chunk_size, top_n)
+print(f'top_errors: {top_errors}')
+
